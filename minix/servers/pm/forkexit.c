6e32d32b4317bb2d7e98ff6194e048f83c7d2682
SrikarGundla06 <180010016@iitdh.ac.in>
2021-01-29 17:26:30 +0530

    Removed create,exited PID, added swap

--- minix/servers/pm/forkexit.c
+++ minix/servers/pm/forkexit.c
@@ -113,7 +113,7 @@ int do_fork()
   new_pid = get_free_pid();
   rmc->mp_pid = new_pid;	/* assign pid to child */
 
-  printf("Minix: PID %d created\n", new_pid);
+  //printf("Minix: PID %d created\n", new_pid);
 
   memset(&m, 0, sizeof(m));
   m.m_type = VFS_PM_FORK;
@@ -244,7 +244,7 @@ int do_exit()
   }
   else {
       exit_proc(mp, m_in.m_lc_pm_exit.status, FALSE /*dump_core*/);
-      printf("Minix: PID %d exited\n", mp->mp_pid);
+      //printf("Minix: PID %d exited\n", mp->mp_pid);
   }
   return(SUSPEND);		/* can't communicate from beyond the grave */
 }
